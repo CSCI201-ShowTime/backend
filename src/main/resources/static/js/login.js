b5f7f1448df94588244e40b33742ac2c614aada4
Cilibiliy <billyxianxu@gmail.com>
2020-11-09 00:18:41 +0800

    Revert "Merge branch 'master' of https://github.com/CSCI201-ShowTime/backend"
    This reverts commit 4eeba514ee1492aa820156e6e227c6680a7cd78d, reversing
    changes made to 91eace1aac65ab6c10e4dab24775c867578e057e.

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -12,11 +12,7 @@ document.querySelector("form").onsubmit = function(event) {
 		document.querySelector(".error").innerHTML = "";
 		document.querySelector(".error").style.display = "none";
 	}
-	
-	var encoded = CryptoJS.MD5(encoded + "ShoWTimE");
-	alert(encoded);
-	document.querySelector("#password").value = encoded;
-	
+
 	// server-side handling
 	loginSever();
 }
