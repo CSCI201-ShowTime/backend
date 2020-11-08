7ab067e8a29b8a83967540fccfe15feca07c92f9
Cilibiliy <billyxianxu@gmail.com>
2020-11-09 00:22:05 +0800

    v0.2.6 mapped all pages in controller

--- src/main/java/showtime/controller/IndexController.java
+++ src/main/java/showtime/controller/IndexController.java
@@ -2,6 +2,7 @@ package showtime.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,10 @@ public class IndexController {
     @RequestMapping("/")
     public String index() {
         return "home.html";
+    }
+
+    @RequestMapping("{path:^.*(?<!\\.html)$}")
+    public String webpage(@PathVariable String path) {
+        return path + ".html";
     }
 }
